7bf420f4117b65e7156d02dc2ffdfc2ecbba95ec
Whitney Sam <whitneysam@Whitneys-MacBook-Pro.local>
2018-07-23 12:08:19 -0700

    Added search bar and google API

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyAirgwYDcXQYz3LBBp_TQnx6Qv1KvrYrJE';
+
+const App = () => {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
