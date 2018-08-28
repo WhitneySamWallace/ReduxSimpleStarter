4ef6e3a1557d0543473434b706d813f92832bc6e
Whitney Sam <whitneysam@Whitneys-MacBook-Pro.local>
2018-08-28 12:49:48 -0700

    initialized state in search_bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,10 +1,17 @@
 //When the user types something in, a search is completed
 
-//Class-based component
+//Class-based component (only class-based components have state)
 
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+    //This is how we initialize state in a class-based component
+    //first and only function called automatically whenever a new instance is created
+    constructor(props) { 
+        super(props); 
+
+        this.state = { term: ''};
+    }
 
     // Every class must have a render function that returns some JSX
     render() {
